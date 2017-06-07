43c038c6546a1dbe4d061a5c4c4c514f47604ec1
ZerxXxes <markus@best-practice.se>
2017-06-07 17:33:12 +0200

    added a lot of stuff

--- observium_installscript.sh
+++ observium_installscript.sh
@@ -11,40 +11,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-if [ -f /etc/apache2/sites-available/000-default.conf ] || [ -f /etc/apache2/sites-available/default ]; then
-    echo -e "${YELLOW}WARNING: Apache default config found, this script will overwrite that config and your current config will be lost${NC}"
-    echo "Continue? (Y/N)"
-    read overwrite_apache
-    if [ $overwrite_apache = "Y" ]; then
-        echo "Apache config will be overwritten."
-    elif [ $overwrite_apache = "N" ]; then
-        echo "Aborting..."
-        exit 1
-    else
-        echo "$overwrite_apache is not a valid option"
-        exit 1
-    fi
-fi
-
-if dpkg --list mysql-server | egrep -q ^ii; then
-    echo -e "${YELLOW}WARNING: A MySQL Server is already installed. Do you know to root password for this server?${NC}"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes )
-                echo "Please enter the MySQL root password and press [ENTER]"
-                read -s mysql_root
-                ;;
-            No )
-                echo "Exiting..."
-                exit 1
-                ;;
-        esac
-    done
-else
-    echo "Choose a MySQL root password"
-    read -s mysql_root
-fi
-
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 
 if [ -f /etc/lsb-release ]; then
@@ -59,13 +25,34 @@ else
     VER=$(uname -r)
 fi
 
+if [[ !$OS =~ ^(Ubuntu|Debian)$ ]]; then
+    echo -e "${RED} [*] ERROR: This installscript does not support this distro, only Debian or Ubuntu supported. Use the manual guide at http://docs.observium.org/install_rhel7/ ${NC}"
+    exit 1
+fi
+
+if [ -f /etc/apache2/sites-available/000-default.conf ] || [ -f /etc/apache2/sites-available/default ]; then
+    echo -e "${YELLOW}WARNING: Apache default config found, this script will overwrite that config and your current config will be lost${NC}"
+    echo "Continue?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes )
+                echo "Apache config will be overwritten..."
+                break
+                ;;
+            No )
+                echo "Exiting..."
+                exit 1
+                ;;
+        esac
+    done
+fi
+
 cat << "EOF"
   ___  _                         _
  / _ \| |__  ___  ___ _ ____   _(_)_   _ _ __ ___
 | | | | '_ \/ __|/ _ \ '__\ \ / / | | | | '_ ` _ \
 | |_| | |_) \__ \  __/ |   \ V /| | |_| | | | | | |
  \___/|_.__/|___/\___|_|    \_/ |_|\__,_|_| |_| |_|
-
 EOF
 echo -e "${GREEN}Welcome to Observium automatic installscript, please choose which verision of Observium you would like to install${NC}"
 echo "1. Observium Community Edition"
@@ -75,12 +62,33 @@ echo -n "(1-3):"
 read observ_ver
 echo "you choose $observ_ver"
 echo " "
-echo "Choose a MySQL password for Observium application"
-read -s mysql_observium
 
+if dpkg --list mysql-server | egrep -q ^ii; then
+    echo -e "${YELLOW}WARNING: A MySQL Server is already installed. Do you know to root password for this server?${NC}"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes )
+                echo "Please enter the MySQL root password and press [ENTER]"
+                read -s mysql_root
+                break
+                ;;
+            No )
+                echo "Exiting..."
+                exit 1
+                ;;
+        esac
+    done
+else
+    echo "Choose a MySQL root password"
+    read -s mysql_root
+fi
+
+#echo "Choose a MySQL password for Observium application"
+#read -s mysql_observium
+mysql_observium="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-15};echo;)"
+echo "got string: $mysql_observium"
 echo "mysql-server mysql-server/root_password password $mysql_root" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $mysql_root" | debconf-set-selections
-
 if [ $OS = "Ubuntu" ] && [ $VER = "16.04" ]; then
    echo -e "${GREEN} [*] We are on Ubuntu 16.04, installing packages...${NC}"
    apt-get -qq install -y libapache2-mod-php7.0 php7.0-cli php7.0-mysql php7.0-mysqli php7.0-gd php7.0-mcrypt php7.0-json php-pear snmp fping mysql-server mysql-client python-mysqldb rrdtool subversion whois mtr-tiny ipmitool graphviz imagemagick apache2
@@ -121,10 +129,8 @@ else
    echo -e "${RED} [*] ERROR: This installscript does not support this distro, only Debian or Ubuntu supported. Use the manual guide at http://docs.observium.org/install_rhel7/ ${NC}"
    exit 1
 fi
-
 echo -e "${GREEN} [*] Creating Observium dir${NC}"
 mkdir -p /opt/observium && cd /opt
-
 if [ $observ_ver = 1 ]; then
    echo -e "${GREEN} [*] Downloading Observium CE and unpacking...${NC}"
    wget -r -nv http://www.observium.org/observium-community-latest.tar.gz
@@ -141,22 +147,16 @@ else
    exit 1
 fi
 cd observium
-
 echo -e "${GREEN} [*] Creating database user for Observium...${NC}"
 mysql -uroot -p"$mysql_root" -e "CREATE DATABASE observium DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
 mysql -uroot -p"$mysql_root" -e "GRANT ALL PRIVILEGES ON observium.* TO 'observium'@'localhost' IDENTIFIED BY '$mysql_observium'"
-
 echo -e "${GREEN} [*] Creating Observium config-file...${NC}"
 sed "s/USERNAME/observium/g" config.php.default > /tmp/installscript.tmp
 sed "s/PASSWORD/$mysql_observium/g" /tmp/installscript.tmp > config.php
-
 ./discovery.php -u
-
-mkdir logs
-
-mkdir rrd
+mkdir -p logs
+mkdir -p rrd
 chown www-data:www-data rrd
-
 read -r -d '' APACHE22 <<- EOM
 <VirtualHost *:80>
     ServerAdmin webmaster@localhost
@@ -181,7 +181,6 @@ read -r -d '' APACHE22 <<- EOM
     ServerSignature On
 </VirtualHost>
 EOM
-
 read -r -d '' APACHE24 <<- EOM
 <VirtualHost *:80>
     ServerAdmin webmaster@localhost
@@ -205,7 +204,6 @@ read -r -d '' APACHE24 <<- EOM
     ServerSignature On
 </VirtualHost>
 EOM
-
 apachever="$(apache2ctl -v)"
 if [[ $apachever == *"Apache/2.4"* ]]; then
   echo -e "${GREEN} [*] Apache version is 2.4, creating config...${NC}"
@@ -219,33 +217,25 @@ else
 fi
 a2enmod rewrite
 apache2ctl restart
-
 echo "Create first time Observium admin user"
 echo -n "Username:"
 read observ_username
 echo -n "Passowrd:"
 read -s observ_password
 ./adduser.php $observ_username $observ_password 10
-
 echo -e "${GREEN} [*] Creating Observium cronjob...${NC}"
 read -r -d '' CRONCONFIG <<- EOM
 # Run a complete discovery of all devices once every 6 hours
 33  */6   * * *   root    /opt/observium/discovery.php -h all >> /dev/null 2>&1
-
 # Run automated discovery of newly added devices every 5 minutes
 */5 *     * * *   root    /opt/observium/discovery.php -h new >> /dev/null 2>&1
-
 # Run multithreaded poller wrapper every 5 minutes
 */5 *     * * *   root    /opt/observium/poller-wrapper.py 4 >> /dev/null 2>&1
-
 # Run housekeeping script daily for syslog, eventlog and alert log
 13 5 * * * root /opt/observium/housekeeping.php -ysel >> /dev/null 2>&1
-
 # Run housekeeping script daily for rrds, ports, orphaned entries in the database and performance data
 47 4 * * * root /opt/observium/housekeeping.php -yrptb >> /dev/null 2>&1
 EOM
 echo $CRONCONFIG > /etc/cron.d/observium
-
-
 echo -e "${GREEN} [*] Installation finished! Use your webbrowser and login to the web interface with the account you just created and add your first device${NC}"
 
